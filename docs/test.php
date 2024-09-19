218ac5fcc0489072ed83d450847a90bac2021422
wasksoftstech <141646352+wasksofts-technology@users.noreply.github.com>
2024-09-19 18:44:37 +0300

    Update test.php

--- docs/test.php
+++ docs/test.php
@@ -13,19 +13,22 @@ $mpesa->config('callback_url', 'https://example.com/webhook.php&type=callback_ur
 $mpesa->config('confirmation_url', 'https://example.com/webhook.php&type=confirmation_url');
 $mpesa->config('validation_url', 'https://example.com/webhook.php&type=validation_url');
 $mpesa->config('initiator_name', '');
-$mpesa->config('initiator_pass', '');
+$mpesa->config('initiator_password', '');
 $mpesa->config('security_credential', '');
 $mpesa->config('result_url', 'https://example.com/webhook.php&type=result_url');
 $mpesa->config('timeout_url', 'https://example.com/webhook.php&type=timeout_url');
-$mpesa->config('env', 'production');
+$mpesa->config('logo_link', 'https://example.com/logo.png');
+$mpesa->config('official_contact', '254716224372');
+$mpesa->config('env', 'production'); //sandbox
 
 echo $mpesa->oauth_token();
-$mpesa->STKPush('500', '254716224372', 'test001', 'test'));
+$mpesa->STKPush('500', '254716224372', 'test001', 'test');
 $mpesa->STKPushQuery('ws_CO_19092024145257708716224372');
-$mpesa->register_url(); 
+$mpesa->register_url();
 $mpesa->b2c('200', 'BusinessPayment', '254708374149', 'payment');
 $mpesa->transaction_status('SIJ159QZWJ', 'payment');
 $mpesa->accountbalance('4', 'remarks');
 $mpesa->reversal('2', '254708374149', '1', 'NCR7S1UXBT');
+$mpesa->generate_qrcode(10, 'TEST10');
 
 var_dump($mpesa->getResponseData());
